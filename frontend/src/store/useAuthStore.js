ca574c9f702b27bd48e0e7e4ac43e8e21abca0de
sherifabdelbast <sherifabdelbast@yahoo.com>
2026-02-18 18:39:02 +0200

    Signup page

---
+++ frontend/src/store/useAuthStore.js
@@ -0,0 +1,46 @@
+import { create } from "zustand";
+import { axiosInstance } from "../lib/axios.js";
+import toast from "react-hot-toast";
+import axios from "axios";
+
+export const useAuthStore = create((set) => ({
+  authUser: null,
+  isSigningUp: false,
+  isLoggingIn: false,
+  isUpdatingProfile: false,
+  isCheckingAuth: true,
+  checkAuth: async () => {
+    try {
+      const res = await axiosInstance.get("/auth/check");
+      set({ authUser: res.data });
+    } catch (error) {
+      set({ authUser: null });
+      console.log(`Error in checkAuth ${error.message}`);
+    } finally {
+      set({ isCheckingAuth: false });
+    }
+  },
+  signup: async (data) => {
+    set({ isSigningUp: true });
+    try {
+      const res = await axiosInstance.post("/auth/signup", data);
+      set({ authUser: res.data });
+      toast.success("Account created successfully");
+    } catch (error) {
+      toast.error(error.response.data.message || "Something went wrong");
+      console.log(`something is wrong in useAuthsStore ${error.message}`);
+    } finally {
+      set({ isSigningUp: false });
+    }
+  },
+  logout: async () => {
+    try {
+      await axiosInstance.post("/auth/logout");
+      set({ authUser: null });
+      toast.success("Logged out successfully");
+    } catch (error) {
+      toast.error(error.response.data.message || "Something went wrong");
+      console.log(`Error in logout ${error.message}`);
+    }
+  },
+}));
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,12 @@
+import daisyui from "daisyui";
+
+export default {
+  content: ["./index.html", "./src/**/*.{html,js,jsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [daisyui],
+  daisyui: {
+    themes: ["light", "dark"],
+  },
+};
